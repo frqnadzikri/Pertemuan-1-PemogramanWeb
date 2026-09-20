cee3953eaf4973da8e2a65bb449006796f581113
Furqon Adzikri <furqonadzikri311@gmail.com>
2026-09-20 22:15:35 +0700

    halaman sementara untuk melihat isi $_SESSION

---
+++ Jobsheet 7/debug_session.php
@@ -0,0 +1,11 @@
+<?php
+// LATIHAN 3: halaman sementara untuk mengintip isi $_SESSION.
+// Hapus file ini setelah latihan selesai.
+$page_title = "Debug Session";
+include __DIR__ . '/includes/header.php';
+?>
+        <section>
+            <h2>Debug Session</h2>
+            <pre><?php echo htmlspecialchars(print_r($_SESSION, true)); ?></pre>
+        </section>
+<?php include __DIR__ . '/includes/footer.php'; ?>
